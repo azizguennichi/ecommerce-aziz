470f01f6adbff4cd130507bc0378684acc87a776
azizguennichi <ezzoguennichi@gmail.com>
2022-11-16 13:42:34 +0100

    first commit

---
+++ src/common/header/Head.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <>
+        <section className='head'>
+            <div className='container d_flex'>
+                <div className='left row'>
+                    <i className='fa fa-phone'></i>
+                    <label>+216 92 156 030</label>
+                    <i className='fa fa-envelope'></i>
+                    <label>Ezzoguennichi@gmail.com</label>
+                </div>
+                <div className='right row RText'>
+                    <label>Theme FAQ's</label>
+                    <label>Need Help</label>
+                    <span>🏴‍☠️</span>
+                    <label>TN</label>
+                    <span>🏴‍☠️</span>
+                    <label>TND</label>
+
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Head
